3bf2d143dd5f4c73ffaff1556ad57e7e79867ae1
Naweed2023 <luxnoori@gmail.com>
2024-04-24 12:40:17 +0200

    basis

---
+++ Master-Arduino.ino
@@ -0,0 +1,24 @@
+// Headers and libraries
+#include <Arduino.h>
+//#include "Joystick.h"
+
+
+// Define the analog pin numbers for the joystick
+//Joystick joystick(A2, A3, 7);
+
+
+
+
+// Setup function
+void setup() {
+    // Initialize serial communication
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    
+    //joystick.read();
+    //joystick.manualMove();
+
+}
