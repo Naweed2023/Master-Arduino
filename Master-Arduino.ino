0bf86bea2c9e4f581a484550684944c994d68527
Naweed2023 <luxnoori@gmail.com>
2024-04-24 14:13:24 +0200

    button voor lampje

--- Master-Arduino.ino
+++ Master-Arduino.ino
@@ -2,6 +2,8 @@
 #include <Arduino.h>
 #include "Joystick.h"
 
+int pinknop = 10;
+int test = 0;
 
 // Define the analog pin numbers for the joystick
 Joystick joystick(A2, A3, 7);
@@ -11,13 +13,15 @@ Joystick joystick(A2, A3, 7);
 
 // Setup function
 void setup() {
+    pinMode(pinknop, OUTPUT);
     // Initialize serial communication
     Serial.begin(9600);
 }
 
 void loop()
 {
-    
+    test = digitalRead(pinknop);
+    Serial.println(test);
     joystick.read();
     joystick.manualMove();
 
